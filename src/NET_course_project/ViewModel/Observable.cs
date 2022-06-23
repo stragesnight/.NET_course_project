c5ed32c6f94c34619096e401663118a6bb15e6ec
win <win@WIN-PC>
2022-06-23 15:19:04 +0300

    Створення класів-помічників для моделі представлення

---
+++ src/NET_course_project/ViewModel/Observable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace NET_course_project.ViewModel
+{
+    // Допоміжний клас, що описує об'єкт, спостерігаючий за змінами в своїх властивостях.
+    public abstract class Observable : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propName = "")
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+    }
+}
---
+++ src/NET_course_project/ViewModel/RelayCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace NET_course_project.ViewModel
+{
+    // Клас, що реалізує інтерфейс ICommand.
+    // Відповідальний за зберігання та виконання методів-команд.
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object param) => _canExecute == null || _canExecute(param);
+        public void Execute(object param) => _execute?.Invoke(param);
+    }
+}
