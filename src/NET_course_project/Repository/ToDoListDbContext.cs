def7cc0536eb5198a55a006b0550073a11b8459a
win <win@WIN-PC>
2022-06-23 15:35:49 +0300

    Створення класів для реалізації доступу до бази даних

---
+++ src/NET_course_project/Repository/ToDoListDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.Entity;
+using NET_course_project.Model;
+
+namespace NET_course_project.Repository
+{
+    /// <summary>
+    /// Клас, що реалізує підтримку EntityFramework.
+    /// Використовується іншими елементами коду для взаємодії з базою даних.
+    /// </summary>
+    public class ToDoListDbContext : DbContext
+    {
+        public DbSet<ToDo> ToDos { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Priority> Priorities { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<ToDo_Tag> ToDos_Tags { get; set; }
+        public DbSet<ToDo_Project> ToDos_Projects { get; set; }
+
+
+        // тимчасова заглушка ініціалізатора
+        public ToDoListDbContext() : base("ToDoListDb")
+        {
+            Database.SetInitializer(new ToDoListDbInitializer());
+        }
+    }
+}
---
+++ src/NET_course_project/Repository/ToDoListDbInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace NET_course_project.Repository
+{
+    /// <summary>
+    /// Клас-ініціалізатор для контексту бази даних.
+    /// </summary>
+    public class ToDoListDbInitializer : DropCreateDatabaseAlways<ToDoListDbContext>
+    {
+        protected override void Seed(ToDoListDbContext context)
+        {
+            // TODO: корректно ініціалізувати базу даних
+            base.Seed(context);
+        }
+    }
+}
