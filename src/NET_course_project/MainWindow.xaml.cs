1f5ddec50eade2846a7eb869d16b682d4da93ff8
win <win@WIN-PC>
2022-06-25 12:31:17 +0300

    Початок роботи над інтерфейсом та моделлю представлення

--- src/NET_course_project/MainWindow.xaml.cs
+++ src/NET_course_project/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using NET_course_project.ViewModel;
 
 namespace NET_course_project
 {
@@ -23,6 +12,7 @@ namespace NET_course_project
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
         }
     }
 }
---
+++ src/NET_course_project/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NET_course_project.Model;
+using NET_course_project.Repository;
+
+namespace NET_course_project.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public ToDoListDbContext DbContext { get; private set; } = null;
+        public ToDo SelectedToDo { get; set; } = null;
+
+
+        public MainWindowViewModel()
+        {
+            DbContext = new ToDoListDbContext();
+        }
+    }
+}
