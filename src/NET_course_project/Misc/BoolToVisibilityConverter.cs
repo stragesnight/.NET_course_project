d612f8d4733d9f1bc65a5815cd3d46afe020473c
stragesnight <alex.spresyo.o@gmail.com>
2022-08-26 18:37:21 +0300

    Правки

--- src/NET_course_project/Misc/BoolToVisibilityConverter.cs
+++ src/NET_course_project/Misc/BoolToVisibilityConverter.cs
@@ -5,7 +5,7 @@ using System.Globalization;
 
 namespace NET_course_project.Misc
 {
-    class BoolToVisibilityConverter : IValueConverter
+    public class BoolToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
